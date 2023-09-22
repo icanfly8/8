c00b0cb5613fe3817dec96f6cf7fc67a57264364
icanfly8 <132223545+icanfly8@users.noreply.github.com>
2023-09-22 18:22:09 +0800

    Update libvio.js

--- js/libvio.js
+++ js/libvio.js
@@ -22,22 +22,23 @@ var rule = {
 	class_parse:'.stui-header__menu li:gt(0):lt(7);a&&Text;a&&href;/(\\d+).html',
 	tab_exclude:'夸克网盘|百度云盘',
 	pagecount:{"27":1},
+	
 	lazy:`js:
-var html = JSON.parse(request(input).match(/r player_.*?=(.*?)</)[1]);
-var url = html.url;
-var from = html.from;
-var next = html.link_next;
-var id = html.id;
-var nid = html.nid;
-var paurl = request(HOST + "/static/player/" + from + ".js").match(/ src="(.*?)'/)[1];
-if (/https/.test(paurl)) {
-	var purl = paurl + url + "&next=" + next + "&id=" + id + "&nid=" + nid;
-	input = {
-		jx: 0,
-		url: request(purl).match(/var .* = '(.*?)'/)[1],
-		parse: 0
-	}
-}`,
+                var html = JSON.parse(request(input).match(/r player_.*?=(.*?)</)[1]);
+                var url = html.url;
+                var from = html.from;
+                var next = html.link_next;
+                var id = html.id;
+                var nid = html.nid;
+                var paurl = request(HOST + "/static/player/" + from + ".js").match(/ src="(.*?)'/)[1];
+                if (/https/.test(paurl)) {
+	                  var purl = paurl + url + "&next=" + next + "&id=" + id + "&nid=" + nid;
+	                  input = {
+		                  jx: 0,
+		                  url: request(purl).match(/var .* = '(.*?)'/)[1],
+		                  parse: 0
+	                  }
+                }`,
 	searchUrl:'/index.php/ajax/suggest?mid=1&wd=**&limit=50',
 	detailUrl:'/detail/fyid.html',
 	搜索:'json:list;name;pic;;id',
