3614a483d48b52fb5dc0c819d21778deb74521de
icanfly8 <132223545+icanfly8@users.noreply.github.com>
2023-09-23 06:22:26 +0800

    Update mp4us.js

--- js/mp4us.js
+++ js/mp4us.js
@@ -38,8 +38,6 @@ var rule = {
 pdfh=jsp.pdfh;pdfa=jsp.pdfa;pd=jsp.pd;
 TABS=[]
 let d = pdfa(html, 'ul.down-list&&li a');
-let tabsa = [];
-let tabsq = [];
 let tabsm = false;
 let tabse = false;
 d.forEach(function(it) {
@@ -95,7 +93,7 @@ if (rule_fetch_params.headers.Cookie.startsWith("http")){
 };
 log('mp4us seach cookie>>>>>>>>>>>>>' + rule_fetch_params.headers.Cookie);
 let _fetch_params = JSON.parse(JSON.stringify(rule_fetch_params));
-let search_html = request( HOST + '/search/' + encodeURIComponent(KEY) + '-1.html', _fetch_params);
+let search_html = request( HOST + '/search/' + encodeURIComponent(KEY) + '-1.html', _fetch_params)
 let d=[];
 let dlist = pdfa(search_html, 'div#list_all li');
 dlist.forEach(function(it){
