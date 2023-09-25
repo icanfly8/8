776136276178eb7f127bba5a065dee2abd2d4cc4
icanfly8 <132223545+icanfly8@users.noreply.github.com>
2023-09-25 23:02:45 +0800

    Update xb6v.js

--- js/xb6v.js
+++ js/xb6v.js
@@ -27,7 +27,7 @@ var rule = {
 		"content": "#post_content&&p:eq(1)&&Text",
 		"tabs": `js:
 			TABS = ["磁力"];
-			let tabs = pdfa(html, '#content&&h3:not(:contains(网盘))');
+			let tabs = pdfa(html, 'div#post_content table tbody tr a');
 			tabs.forEach((it) => {
 				TABS.push(pdfh(it, "body&&Text").replace('播放地址','道长在线').replace('（无插件 极速播放）','一').replace('（无需安装插件）','二'))
 			});
