fb01e1f78756e3282a95d7b3a17018d7e0e71f8c
icanfly8 <132223545+icanfly8@users.noreply.github.com>
2023-09-28 01:06:57 +0800

    Update czzy_open.js

--- js/czzy_open.js
+++ js/czzy_open.js
@@ -73,7 +73,7 @@ async function home(filter) {
             type_name: s.children[0].data,
         };
     });
-    const sortName = ['电影', '电视剧', '国产剧', '美剧', '日剧', '韩剧', '海外剧（其他）', '华语电影', '欧美电影', '日本电影', '韩国电影', '俄罗斯电影', '加拿大电影', '印度电影', '动画'];
+    const sortName = ['电影', '电视剧', '国产剧', '美剧', '韩剧', '日剧', '海外剧（其他）', '华语电影', '印度电影', '日本电影', '欧美电影', '韩国电影', '动画', '俄罗斯电影', '加拿大电影'];
     classes = _.sortBy(classes, (c) => {
         const index = sortName.indexOf(c.type_name);
         return index === -1 ? sortName.length : index;
