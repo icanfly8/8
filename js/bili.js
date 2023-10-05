c2186f3ac593aa7ada351f8e17114736194d1d26
icanfly8 <132223545+icanfly8@users.noreply.github.com>
2023-10-05 18:28:06 +0800

    Update bili.js

--- js/bili.js
+++ js/bili.js
@@ -76,8 +76,8 @@ var rule = {
         input = result
     `,
     double:false,
-    //推荐:'*',
-    
+    推荐:'*',
+    /*
     推荐:`js:
         function stripHtmlTag(src) {
             return src.replace(/<\\/?[^>]+(>|$)/g, '').replace(/&.{1,5};/g, '').replace(/\\s{2,}/g, ' ');
@@ -171,7 +171,7 @@ var rule = {
         VODS = videos
     `,
     
-    
+    */
     一级:`js:
         function stripHtmlTag(src) {
             return src.replace(/<\\/?[^>]+(>|$)/g, '').replace(/&.{1,5};/g, '').replace(/\\s{2,}/g, ' ');
